97394274346c70f1288d50f485c01ea69e64e84b
y-hryk <dev.hy630823@gmail.com>
2016-08-25 01:22:26 +0900

    add project

---
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by yamaguchi on 2016/08/25.
+//  Copyright © 2016年 h.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
